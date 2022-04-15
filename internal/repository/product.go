ce8902d15f78163a8760c32587cc5ae004557512
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-15 22:25:16 +0300

    Product repository functions are implemented

---
+++ internal/repository/product.go
@@ -0,0 +1,84 @@
+package repo
+
+import (
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/helper"
+)
+
+type IProductRepository interface {
+	CreateProduct(*models.Product) (*models.Product, error)
+	FetchAllProducts() (*[]models.Product, error)
+	FetchProduct(string) (*models.Product, error)
+	FetchProductsOfSpecificCategory(string) (*[]models.Product, error)
+}
+
+func (r *Repository) CreateProduct(p *models.Product) (*models.Product, error) {
+
+	// DB query to create
+	if err := r.DB.Create(&p).Error; err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (r *Repository) FetchAllProducts(pag *helper.Pagination) (*[]models.Product, error) {
+	var products = &[]models.Product{}
+
+	offset := (pag.Page) * pag.Limit
+	queryBuilder := r.DB.Limit(int(pag.Limit)).Offset(int(offset)).Order(pag.Sort)
+
+	// DB query to get
+	if err := queryBuilder.Preload("Category").Find(&products).Error; err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (r *Repository) FetchProduct(id string) (*models.Product, error) {
+	var product = &models.Product{}
+
+	// DB query to get
+	if err := r.DB.Preload("Category").Where(&models.Product{ID: id}).First(&product).Error; err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (r *Repository) FetchProductsOfSpecificCategory(id string, pag *helper.Pagination) (*[]models.Product, error) {
+
+	// DB query to get
+	category, err := r.FetchCategory(id)
+	if err != nil {
+		return nil, err
+	}
+
+	var products = &[]models.Product{}
+
+	offset := (pag.Page) * pag.Limit
+	queryBuilder := r.DB.Limit(int(pag.Limit)).Offset(int(offset)).Order(pag.Sort)
+
+	// DB query
+	queryBuilder.Model(&category).Association("Products").Find(&products)
+	return products, nil
+}
+
+func (r *Repository) UpdateProduct(p *models.Product) (*models.Product, error) {
+
+	// DB query to update
+	if err := r.DB.Save(&p).Error; err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (r *Repository) DeleteProduct(id string) error {
+	product, err := r.FetchProduct(id)
+	if err != nil {
+		return err
+	}
+
+	if result := r.DB.Unscoped().Delete(&product); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
