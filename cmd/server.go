683a485b2699991864d8221d0c4fce9b2eb85f75
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-15 22:59:39 +0300

    Router groups, repository, handlers and services are started

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/database"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/handler"
+	repo "github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/repository"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/service"
 	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/config"
 	"github.com/gin-gonic/gin"
 )
@@ -21,5 +24,24 @@ func InitServer(cfg *config.Config) {
 	// Router groups
 	rootRouter := r.Group(cfg.ServerConfig.RoutePrefix)
 	productRouter := rootRouter.Group("/")
+	categoryRouter := rootRouter.Group("/category")
+	userRouter := rootRouter.Group("/user")
+
+	// Repository
+	repo := repo.NewRepository(DB)
+	repo.Migrations()
+
+	// Service and Handlers
+	productService := service.NewProductService(repo)
+	handler.NewProductHandler(productRouter, productService, cfg)
+
+	categoryService := service.NewCategoryService(repo)
+	handler.NewCategoryHandler(categoryRouter, categoryService)
+
+	userService := service.NewUserService(repo)
+	handler.NewUserHandler(userRouter, userService, cfg)
+
+	// Run localhost:8080
+	r.Run(":8080")
 
 }
