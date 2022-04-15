a5788abf3dfdafd88b636b121ff712d70103566f
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-15 20:55:21 +0300

    Product struct implemented

---
+++ internal/entity/models/product.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+	ID          string  `json:"id" gorm:"primaryKey"`
+	Name        string  `json:"name" gorm:"uniqueIndex; not null"`
+	Description string  `json:"description" gorm:"not null"`
+	Price       float64 `json:"price" gorm:"not null"`
+	Stock       int64   `json:"stock" gorm:"not null"`
+	Brand       string  `json:"brand" gorm:"not null"`
+
+	Category          []Category        `gorm:"many2many:products_categories;" json:"category"`
+	ProductCategories []ProductCategory `gorm:"foreignkey:ProductId"`
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) error {
+	if p.ID == "" {
+		p.ID = uuid.NewString()
+	}
+	return nil
+}
