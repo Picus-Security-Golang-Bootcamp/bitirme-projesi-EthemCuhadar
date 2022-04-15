8959559cb7093b34c72713e5d1e1b6e04856bded
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-15 22:20:44 +0300

    Repository of project is ready

---
+++ internal/repository/repo.go
@@ -0,0 +1,25 @@
+package repo
+
+import (
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{DB: db}
+}
+
+func (r *Repository) Migrations() {
+	productPrototype := models.Product{}
+	categoryPrototype := models.Category{}
+	userPrototype := models.User{}
+	r.DB.AutoMigrate(
+		productPrototype,
+		categoryPrototype,
+		userPrototype,
+	)
+}
