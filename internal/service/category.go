9f3b89a384fa8885c1a7fc0a4093fd32fe750fca
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-15 22:31:38 +0300

    Category service is ready

---
+++ internal/service/category.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/dtos"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+	repo "github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/repository"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/helper"
+)
+
+// CategoryService is a struct which will be the bridge between handler and repository.
+// It contains repository field.
+type CategoryService struct {
+	repository *repo.Repository
+}
+
+// NewCategoryService takes a repository and returns a new CategoryService.
+func NewCategoryService(repo *repo.Repository) *CategoryService {
+	return &CategoryService{repository: repo}
+}
+
+// CreateCategory Category model and pass the model to repository. Afterwards, it takes
+// Category model from repository and convert into response Category dto model. Finally,
+// returns the response Category dto model.
+func (cs *CategoryService) CreateCategory(c *models.Category) (*dtos.ResponseCategoryDto, error) {
+	category, err := cs.repository.CreateCategory(c)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := helper.ConvertCategoryModelToResponseCategoryDto(category)
+	return categoryResponse, nil
+}
+
+func (cs *CategoryService) FetchAllCategories(pag *helper.Pagination) (*dtos.ResponseAllCategoriesDto, error) {
+
+	categories, err := cs.repository.FetchAllCategories(pag)
+	if err != nil {
+		return nil, err
+	}
+
+	categoryListResponse := helper.ConvertCategoryModelsToResponseAllCategoriesDto(*categories)
+	if err != nil {
+		return nil, err
+	}
+	return categoryListResponse, nil
+}
+
+func (cs *CategoryService) FetchCategory(id string) (*dtos.ResponseCategoryDto, error) {
+	category, err := cs.repository.FetchCategory(id)
+	if err != nil {
+		return nil, err
+	}
+	categoryResponse := helper.ConvertCategoryModelToResponseCategoryDto(category)
+	return categoryResponse, nil
+}
