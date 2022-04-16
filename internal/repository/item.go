a73d500b09246cd334afbc9fdce75a4c58415895
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-16 21:00:23 +0300

    Item repository is ready for CRUD operations

---
+++ internal/repository/item.go
@@ -0,0 +1,39 @@
+package repo
+
+import "github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+
+func (r *Repository) FetchItem(item_id string) (*models.Item, error) {
+	var item = &models.Item{}
+
+	// DB query to get
+	if err := r.DB.Where(&models.Item{ID: item_id}).First(&item).Error; err != nil {
+		return nil, err
+	}
+	return item, nil
+}
+
+func (r *Repository) UpdateItem(i *models.Item) error {
+	// DB query to update
+	if err := r.DB.Save(&i).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *Repository) DeleteItem(item_id string) error {
+	item, err := r.FetchItem(item_id)
+	if err != nil {
+		return err
+	}
+	if result := r.DB.Unscoped().Delete(&item); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func (r *Repository) CreateItem(i *models.Item) (*models.Item, error) {
+	if err := r.DB.Create(&i).Error; err != nil {
+		return nil, err
+	}
+	return i, nil
+}
