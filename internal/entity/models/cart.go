b32a70d898aecaf06816f839ddf797f3ee964097
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-16 20:59:06 +0300

    Cart and Item models are created and relative functions of cart model are implemented

---
+++ internal/entity/models/cart.go
@@ -0,0 +1,89 @@
+package models
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+var minCartPrice float64 = 0
+
+type Cart struct {
+	gorm.Model
+	ID     string  `json:"id"`
+	UserID string  `json:"userId"`
+	Items  []Item  `json:"items"`
+	Price  float64 `json:"price"`
+}
+
+type Item struct {
+	CartId   string  `json:"cartId"`
+	ID       string  `json:"id"`
+	Price    float64 `json:"price"`
+	Quantity uint64  `json:"quantity"`
+}
+
+func (c *Cart) BeforeCreate(tx *gorm.DB) error {
+	c.ID = uuid.NewString()
+	return nil
+}
+
+func (c *Cart) AddItem(i *Item) error {
+	zap.L().Debug("models.cart.AddItem")
+
+	_, item := c.SearchItemByID(i.ID)
+	if item != nil {
+		zap.L().Debug("Item already exists")
+		return errors.New("Item already exists")
+	}
+	// item = &Item{
+	// 	ID:       i.ID,
+	// 	Price:    i.Price,
+	// 	Quantity: i.Quantity,
+	// }
+	c.Items = append(c.Items, *i)
+	return nil
+}
+
+func (c *Cart) UpdateItem(i *Item, quantity int) (err error) {
+	if index, item := c.SearchItemByID(i.ID); index != -1 {
+		item.Quantity = uint64(quantity)
+	} else {
+		return errors.New("Item not found")
+	}
+	return
+}
+
+func (c *Cart) RemoveItem(i *Item) error {
+	if index, _ := c.SearchItemByID(i.ID); index != -1 {
+		c.Items = append(c.Items[:index], c.Items[index+1:]...)
+	}
+	return errors.New("Item not found")
+}
+
+func (c *Cart) ValidateCart() error {
+	totalPrice := CalculateTotalPrice(c)
+	if totalPrice < minCartPrice {
+		return errors.New("total cart price must be greater than min price")
+	}
+	c.Price = totalPrice
+	return nil
+}
+
+func CalculateTotalPrice(c *Cart) (totalPrice float64) {
+	for _, item := range c.Items {
+		totalPrice += float64(item.Price) * float64(item.Quantity)
+	}
+	return
+}
+
+func (c *Cart) SearchItemByID(id string) (int, *Item) {
+	for i, item := range c.Items {
+		if item.ID == id {
+			return i, &item
+		}
+	}
+	return -1, nil
+}
