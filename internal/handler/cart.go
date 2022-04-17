626e2fe6625296d7ece93364773539ae7b44e46d
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-17 13:56:13 +0300

    Order of a user can be created and deleted with functions

--- internal/handler/cart.go
+++ internal/handler/cart.go
@@ -7,8 +7,10 @@ import (
 	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/service"
 	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/config"
 	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/httpErrors"
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/pkg/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/go-openapi/strfmt"
+	"go.uber.org/zap"
 )
 
 type CartHandler struct {
@@ -21,13 +23,17 @@ func NewCartHandler(r *gin.RouterGroup, service *service.CartService, cfg *confi
 		service: service,
 		config:  cfg,
 	}
-
-	r.POST("/user/:user_id/cart/create", ch.CreateCart)
-	r.DELETE("/user/:user_id/cart/:cart_id", ch.DeleteCart)
-	r.GET("user/:user_id/cart/:cart_id", ch.FetchCart)
-	r.POST("user/:user_id/cart/:cart_id", ch.AddItemToCart)
-	r.POST("user/:user_id/cart/:cart_id/item/:item_id", ch.UpdateItem)
-	r.DELETE("user/:user_id/cart/:cart_id/item/:item_id", ch.DeleteItem)
+	r.Use(middleware.JWTMiddleware(cfg), middleware.UserMiddleware(cfg))
+	{
+		r.POST("/user/:user_id/cart/create", ch.CreateCart)
+		r.DELETE("/user/:user_id/cart/:cart_id", ch.DeleteCart)
+		r.GET("user/:user_id/cart/:cart_id", ch.FetchCart)
+		r.POST("user/:user_id/cart/:cart_id", ch.AddItemToCart)
+		r.POST("user/:user_id/cart/:cart_id/item/:item_id", ch.UpdateItem)
+		r.DELETE("user/:user_id/cart/:cart_id/item/:item_id", ch.DeleteItem)
+		r.POST("user/:user_id/cart/:cart_id/complete", ch.CompleteOrder)
+		r.DELETE("/user/:user_id/cart/:cart_id/cancel", ch.DeleteCart)
+	}
 
 }
 
@@ -85,19 +91,24 @@ func (ch *CartHandler) AddItemToCart(c *gin.Context) {
 	itemBody := dtos.Item{}
 	cart_id := c.Param("cart_id")
 
+	zap.L().Debug("handler.cart.additemtocart")
+
 	// Binding
 	if err := c.Bind(&itemBody); err != nil {
+		zap.L().Debug("handler.cart.additemtocart error 1")
 		c.JSON(httpErrors.ErrorResponse(httpErrors.CannotBindGivenData))
 	}
 
 	// Validation
 	if err := itemBody.Validate(strfmt.NewFormats()); err != nil {
+		zap.L().Debug("handler.cart.additemtocart error 2")
 		c.JSON(httpErrors.ErrorResponse(err))
 		return
 	}
 
 	cartResponse, err := ch.service.AddItemToCart(&itemBody, cart_id)
 	if err != nil {
+		zap.L().Debug("handler.cart.additemtocart error 2")
 		c.JSON(httpErrors.ErrorResponse(err))
 	}
 
@@ -139,4 +150,23 @@ func (ch *CartHandler) DeleteItem(c *gin.Context) {
 
 	// Serialization into response body
 	c.JSON(http.StatusOK, cartResponse)
+}
+
+func (ch *CartHandler) CompleteOrder(c *gin.Context) {
+
+	cart_id := c.Param("cart_id")
+	cartResponse, err := ch.service.CompleteOrder(cart_id)
+	if err != nil {
+		c.JSON(httpErrors.ErrorResponse(err))
+	}
+	c.JSON(http.StatusOK, cartResponse)
+}
+
+func (ch *CartHandler) CancelOrder(c *gin.Context) {
+	cart_id := c.Param("cart_id")
+	err := ch.service.CancelOrder(cart_id)
+	if err != nil {
+		c.JSON(httpErrors.ErrorResponse(err))
+	}
+	c.JSON(http.StatusOK, nil)
 }
