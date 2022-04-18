48fb0ac724eb25e60f30bbfdded6d849a43f3a2f
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-18 08:48:29 +0300

    Cart repo zapped

--- internal/repository/cart.go
+++ internal/repository/cart.go
@@ -1,14 +1,19 @@
 package repo
 
-import "github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+import (
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+	"go.uber.org/zap"
+)
 
 // CreateCart gets data from database and sends them into service, if there are no errors
 func (r *Repository) CreateCart(c *models.Cart) (*models.Cart, error) {
+	zap.L().Debug("repo.cart.CreateCart")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	// DB query
 	if err := r.DB.Create(&c).Error; err != nil {
+		zap.L().Debug("repo.cart.CreateCart Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return c, nil
@@ -16,12 +21,14 @@ func (r *Repository) CreateCart(c *models.Cart) (*models.Cart, error) {
 
 // CreateCart gets data from database and sends them into service, if there are no errors
 func (r *Repository) FetchCart(cart_id string) (*models.Cart, error) {
+	zap.L().Debug("repo.cart.FetchCart")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	var cart = &models.Cart{}
 
 	// DB query
 	if err := r.DB.Preload("Items").Where(&models.Cart{ID: cart_id}).First(&cart).Error; err != nil {
+		zap.L().Debug("repo.cart.FetchCart Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return cart, nil
@@ -29,8 +36,10 @@ func (r *Repository) FetchCart(cart_id string) (*models.Cart, error) {
 
 // CreateCart gets data from database and sends them into service, if there are no errors
 func (r *Repository) DeleteCart(cart_id string) error {
+	zap.L().Debug("repo.cart.DeleteCart")
 	cart, err := r.FetchCart(cart_id)
 	if err != nil {
+		zap.L().Debug("repo.cart.DeleteCart Error 1", zap.Reflect("error:", err))
 		return err
 	}
 	r.mu.Lock()
@@ -43,6 +52,7 @@ func (r *Repository) DeleteCart(cart_id string) error {
 
 	// DB query
 	if result := r.DB.Delete(&cart); result.Error != nil {
+		zap.L().Debug("repo.cart.DeleteCart Error 2", zap.Reflect("error:", result.Error))
 		return result.Error
 	}
 	return nil
@@ -50,11 +60,13 @@ func (r *Repository) DeleteCart(cart_id string) error {
 
 // CreateCart gets data from database and sends them into service, if there are no errors
 func (r *Repository) UpdateCart(c *models.Cart) (*models.Cart, error) {
+	zap.L().Debug("repo.cart.UpdateCart")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	// DB query to update
 	if err := r.DB.Save(&c).Error; err != nil {
+		zap.L().Debug("repo.cart.UpdateCart Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return c, nil
@@ -62,12 +74,14 @@ func (r *Repository) UpdateCart(c *models.Cart) (*models.Cart, error) {
 
 // CreateCart gets data from database and sends them into service, if there are no errors
 func (r *Repository) GetAllOrders(user_id string) (*[]models.Cart, error) {
+	zap.L().Debug("repo.cart.GetAllOrders")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	var carts = &[]models.Cart{}
 
 	// DB query
 	if err := r.DB.Preload("Items").Where("user_id = ? AND is_ordered = ?", user_id, true).Find(&carts).Error; err != nil {
+		zap.L().Debug("repo.cart.GetAllOrders Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return carts, nil
