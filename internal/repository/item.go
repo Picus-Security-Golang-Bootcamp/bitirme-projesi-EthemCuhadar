b038b6d2c5d1538d3247d6104b01508c18260f73
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-18 08:52:57 +0300

    Item repo zapped

--- internal/repository/item.go
+++ internal/repository/item.go
@@ -1,15 +1,20 @@
 package repo
 
-import "github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+import (
+	"github.com/Picus-Security-Golang-Bootcamp/bitirme-projesi-EthemCuhadar/internal/entity/models"
+	"go.uber.org/zap"
+)
 
 // FetchItem gets data from database and sends them into service, if there are no errors
 func (r *Repository) FetchItem(item_id string) (*models.Item, error) {
+	zap.L().Debug("repo.cart.FetchItem")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 	var item = &models.Item{}
 
 	// DB query to get
 	if err := r.DB.Where(&models.Item{ID: item_id}).First(&item).Error; err != nil {
+		zap.L().Debug("repo.cart.FetchItem Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return item, nil
@@ -17,11 +22,13 @@ func (r *Repository) FetchItem(item_id string) (*models.Item, error) {
 
 // UpdateItem gets data from database and sends them into service, if there are no errors
 func (r *Repository) UpdateItem(i *models.Item) error {
+	zap.L().Debug("repo.cart.UpdateItem")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	// DB query to update
 	if err := r.DB.Save(&i).Error; err != nil {
+		zap.L().Debug("repo.cart.UpdateItem Error 1", zap.Reflect("error:", err))
 		return err
 	}
 	return nil
@@ -29,10 +36,12 @@ func (r *Repository) UpdateItem(i *models.Item) error {
 
 // DeleteItem gets data from database and sends them into service, if there are no errors
 func (r *Repository) DeleteItem(item_id string) error {
+	zap.L().Debug("repo.cart.DeleteItem")
 
 	// DB query
 	item, err := r.FetchItem(item_id)
 	if err != nil {
+		zap.L().Debug("repo.cart.DeleteItem Error 1", zap.Reflect("error:", err))
 		return err
 	}
 
@@ -41,6 +50,7 @@ func (r *Repository) DeleteItem(item_id string) error {
 
 	// DB query
 	if result := r.DB.Unscoped().Delete(&item); result.Error != nil {
+		zap.L().Debug("repo.cart.DeleteItem Error 2", zap.Reflect("error:", err))
 		return result.Error
 	}
 	return nil
@@ -48,11 +58,13 @@ func (r *Repository) DeleteItem(item_id string) error {
 
 // FetchCategory gets data from database and sends them into service, if there are no errors
 func (r *Repository) CreateItem(i *models.Item) (*models.Item, error) {
+	zap.L().Debug("repo.cart.CreateItem")
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	// DB query
 	if err := r.DB.Create(&i).Error; err != nil {
+		zap.L().Debug("repo.cart.CreateItem Error 1", zap.Reflect("error:", err))
 		return nil, err
 	}
 	return i, nil
