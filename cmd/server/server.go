53929cd0f0a11da297e444fcec2c41c2bc5969b6
EthemCuhadar <iethemcuhadar@gmail.com>
2022-04-18 04:53:52 +0300

    Final comments

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -15,18 +15,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// InitServer is the function which will take configuration parameters and starts
+// server connections with database.
 func InitServer(cfg *config.Config) {
 
 	// Database connection
 	DB := database.Connect(cfg)
 	defer database.Close(DB)
 
-	// Set gin mode
+	// Set gin mode as release mode
 	gin.SetMode(gin.ReleaseMode)
 
 	// Gin initialize
 	r := gin.Default()
 
+	// Server parameters
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%s", cfg.ServerConfig.Port),
 		Handler:      r,
@@ -45,28 +48,30 @@ func InitServer(cfg *config.Config) {
 	repo := repo.NewRepository(DB)
 	repo.Migrations()
 
-	// Service and Handlers
+	// Product Service and Handler
 	productService := service.NewProductService(repo)
 	handler.NewProductHandler(productRouter, productService, cfg)
 
+	// Category Service and Handler
 	categoryService := service.NewCategoryService(repo)
 	handler.NewCategoryHandler(categoryRouter, categoryService, cfg)
 
+	// User Service and Handler
 	userService := service.NewUserService(repo)
 	handler.NewUserHandler(userRouter, userService, cfg)
 
+	// Cart Service and Handler
 	cartService := service.NewCartService(repo)
 	handler.NewCartHandler(cartRouter, cartService, cfg)
 
+	// Listen and Serve
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
 
+	// Graceful shotdown for running processes
 	graceful.ShutdownGin(srv, time.Duration(cfg.ServerConfig.TimeoutSecs))
-
-	// Run localhost:8080
-	// r.Run(":8080")
 
 }
